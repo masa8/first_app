d02d692c709e765fa1e1a39127719f965fc3fcb6
masa <masa.shimomatsu@gmail.com>
2014-03-23 08:01:34 +0900

    add one line for heroku

--- config/application.rb
+++ config/application.rb
@@ -59,4 +59,7 @@ module FirstApp
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
+
+    # for Heroku
+    config.assets.initialize_on_precompile = false
 end
